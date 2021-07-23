5eac5805818c34cf736290edec12919558c6a15e
Gongash92 <zarko.stevanovic092@gmail.com>
2021-07-23 12:40:37 +0200

    ispravka

--- src/pages/PostaniteSaradnik.vue
+++ src/pages/PostaniteSaradnik.vue
@@ -463,7 +463,7 @@ export default {
     z() {
       if (process.isClient)
         // this will run when linking directly or after a refresh
-        window.location.href = "https://www.alojanaklik.rs//contact/";
+        window.location.href = "https://www.alojanaklik.rs/contact/";
     },
   },
   mounted() {
