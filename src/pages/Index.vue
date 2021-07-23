ce0b8d565249817835b38a0aeaa3b200a738905d
Gongash92 <zarko.stevanovic092@gmail.com>
2021-07-23 15:46:11 +0200

    tekst ispravka

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,7 +6,7 @@
           <div class="col-5@md position-relative z-index-1">
             <div class="text-component">
               <h1 class="text-xxxl feature-v4__text-offset@md">
-                Forever Living
+                Forever Living <br>
                 Otkrijte posao kojim se možete ponositi
               </h1>
               <div>
