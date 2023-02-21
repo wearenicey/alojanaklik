05c7455c93399e48272ef67a1b5d8e4c9575c61f
Goran Babarogic <gbabarogic@gmail.com>
2023-02-21 13:32:35 +0100

    fix

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -19,31 +19,29 @@
 						</a>
 					</div>
 
-					
-
 					<ul class="f-header__list flex-grow flex-basis-0 justify-end@md">
-						<ul class="f-header__list flex-grow flex-basis-0 justify-center@md">
-						<li class="f-header__item">
-							<g-link to="/proizvodi/" class="f-header__link">
-								<span>Proizvodi</span>
-								<svg class="f-header__dropdown-icon icon" aria-hidden="true" viewBox="0 0 12 12">
-									<path d="M9.943,4.269A.5.5,0,0,0,9.5,4h-7a.5.5,0,0,0-.41.787l3.5,5a.5.5,0,0,0,.82,0l3.5-5A.5.5,0,0,0,9.943,4.269Z" />
-								</svg>
-							</g-link>
+						<ul class="f-header__listflex-basis-0 justify-center@md">
+							<li class="f-header__item">
+								<g-link to="/proizvodi/" class="f-header__link">
+									<span>Proizvodi</span>
+									<svg class="f-header__dropdown-icon icon" aria-hidden="true" viewBox="0 0 12 12">
+										<path d="M9.943,4.269A.5.5,0,0,0,9.5,4h-7a.5.5,0,0,0-.41.787l3.5,5a.5.5,0,0,0,.82,0l3.5-5A.5.5,0,0,0,9.943,4.269Z" />
+									</svg>
+								</g-link>
 
-							<ul class="f-header__dropdown">
-								<li><g-link to="/dodaci-ishrani/" class="f-header__dropdown-link">Dodaci ishrani</g-link></li>
-								<li><a href="/preparati-za-higijenu/" class="f-header__dropdown-link">Lična higijena</a></li>
-								<li><a href="/mrsavljenje/" class="f-header__dropdown-link">Mršavljenje</a></li>
-								<li><a href="/napici/" class="f-header__dropdown-link">Napici</a></li>
-								<li><a href="/preparati-za-negu-koze/" class="f-header__dropdown-link">Nega kože</a></li>
-								<li><a href="/pcelinji-proizvodi/" class="f-header__dropdown-link">Pčelinji proizvodi</a></li>
-							</ul>
-						</li>
-					<!--	<li class="f-header__item"><g-link to="/postanite-saradnik/" class="f-header__link">Postanite saradnik</g-link></li>
+								<ul class="f-header__dropdown">
+									<li><g-link to="/dodaci-ishrani/" class="f-header__dropdown-link">Dodaci ishrani</g-link></li>
+									<li><a href="/preparati-za-higijenu/" class="f-header__dropdown-link">Lična higijena</a></li>
+									<li><a href="/mrsavljenje/" class="f-header__dropdown-link">Mršavljenje</a></li>
+									<li><a href="/napici/" class="f-header__dropdown-link">Napici</a></li>
+									<li><a href="/preparati-za-negu-koze/" class="f-header__dropdown-link">Nega kože</a></li>
+									<li><a href="/pcelinji-proizvodi/" class="f-header__dropdown-link">Pčelinji proizvodi</a></li>
+								</ul>
+							</li>
+							<!--	<li class="f-header__item"><g-link to="/postanite-saradnik/" class="f-header__link">Postanite saradnik</g-link></li>
 						<li class="f-header__item"><g-link to="/istorija-kompanije/" class="f-header__link">Istorija kompanije</g-link></li>
 						<li class="f-header__item"><g-link to="/blog/" class="f-header__link">Blog </g-link></li> -->
-					</ul>
+						</ul>
 						<li class="f-header__item"><a href="/kontakt/" class="f-header__btn btn btn--subtle">Kontakt</a></li>
 					</ul>
 				</div>
