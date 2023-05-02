023f119b5ed411e8e055fd2f421e6aae1f2fbd0f
dejan.krcmar <dejan.krcmar@gmail.com>
2023-05-02 10:58:37 +0200

    uklonjen sidebar

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -1,109 +1,54 @@
 <template>
-	<Layout>
-		<article class="padding-y-xxl">
-			<header class="container max-width-xs margin-bottom-lg">
-				<div
-					class="
+  <Layout>
+    <article class="padding-y-xxl">
+      <header class="container max-width-xs margin-bottom-lg">
+        <div
+          class="
             text-component text-center
             line-height-lg
             v-space-md
             margin-bottom-md
           "
-				>
-					<h1>{{ $page.post.title }}</h1>
-					<p class="color-contrast-medium text-md">{{ $page.post.excerpt }}</p>
-				</div>
+        >
+          <h1>{{ $page.post.title }}</h1>
+          <p class="color-contrast-medium text-md">{{ $page.post.excerpt }}</p>
+        </div>
 
-				<div class="flex justify-center">
-					<div class="author__content text-component v-space-xxs">
-						<!-- <p class="text-sm color-contrast-medium">
-							<time class="text-sm color-inherit" :datetime="$page.post.datetime">{{ $page.post.humanTime }}</time
-							>&mdash; {{ $page.post.timeToRead }} min čitanja
-						</p> -->
-					</div>
-				</div>
-			</header>
+        <div class="flex justify-center">
+          <div class="author__content text-component v-space-xxs">
+            <!-- <p class="text-sm color-contrast-medium">
+              <time class="text-sm color-inherit" :datetime="$page.post.datetime">{{ $page.post.humanTime }}</time
+              >&mdash; {{ $page.post.timeToRead }} min čitanja
+            </p> -->
+          </div>
+        </div>
+      </header>
 
-			<figure class="container max-width-md margin-bottom-lg">
-				<g-image :src="require(`!!assets-loader!@img/${$page.post.image}`)" class="width-100%" :alt="$page.post.alt"></g-image>
-			</figure>
+      <figure class="container max-width-md margin-bottom-lg">
+        <g-image :src="require(`!!assets-loader!@img/${$page.post.image}`)" class="width-100%" :alt="$page.post.alt"></g-image>
+      </figure>
 
-			<div class="container max-width-adaptive-xl">
-				<div class="grid gap-md">
-					<div class="col-8@lg">
-						<VueRemarkContent />
-					</div>
-					<div class="col-4@md">
-						<div class="stories">
-							<h3 class="stories__legend">Popular stories</h3>
-
-							<ol class="stories__list">
-								<li class="stories__story">
-									<a href="#0" class="stories__img-wrapper">
-										<figure>
-											<img src="" alt="Image description" />
-										</figure>
-									</a>
-
-									<div class="stories__text text-component line-height-md text-space-y-sm">
-										<h4 class="stories__headline"><a href="#0">It was going to be a lonely trip.</a></h4>
-									</div>
-								</li>
-
-								<li class="stories__story">
-									<a href="#0" class="stories__img-wrapper">
-										<figure>
-											<img src="" alt="Image description" />
-										</figure>
-									</a>
-
-									<div class="stories__text text-component line-height-md text-space-y-sm">
-										<h4 class="stories__headline"><a href="#0">I watched the storm, so beautiful yet terrific.</a></h4>
-									</div>
-								</li>
-
-								<li class="stories__story">
-									<a href="#0" class="stories__img-wrapper">
-										<figure>
-											<img src="" alt="Image description" />
-										</figure>
-									</a>
-
-									<div class="stories__text text-component line-height-md text-space-y-sm">
-										<h4 class="stories__headline"><a href="#0">The face of the moon was in shadow.</a></h4>
-									</div>
-								</li>
-
-								<li class="stories__story">
-									<a href="#0" class="stories__img-wrapper">
-										<figure>
-											<img src="" alt="Image description" />
-										</figure>
-									</a>
-
-									<div class="stories__text text-component line-height-md text-space-y-sm">
-										<h4 class="stories__headline"><a href="#0">My two natures had memory in common.</a></h4>
-									</div>
-								</li>
-							</ol>
-						</div>
-					</div>
-				</div>
-			</div>
-		</article>
-	</Layout>
+      <div class="container max-width-adaptive-sm">
+        <div class="grid gap-md">
+          <div class="col">
+            <VueRemarkContent />
+          </div>
+        </div>
+      </div>
+    </article>
+  </Layout>
 </template>
 
 <!-- GraphQl upit za blog postove <-->
 <page-query>
   query Post ($path: String!) {
   post: post (path: $path) {
-		path
+    path
       title
       image
-			alt
+      alt
       image_caption
-			tagsMeta
+      tagsMeta
       excerpt
       imageTwo {
         path
@@ -118,74 +63,76 @@
 
 <script>
 export default {
-	//meta desc dinamicni
-	metaInfo() {
-		return {
-			title: this.$page.post.title + " - Aloja Vera",
-			titleTemplate: this.$page.post.title + " - Aloja Vera",
+  //meta desc dinamicni
+  metaInfo() {
+    return {
+      title: this.$page.post.title + " - Aloja Vera",
+      titleTemplate: this.$page.post.title + " - Aloja Vera",
 
-			meta: [
-				{ name: "description", content: this.$page.post.tagsMeta },
+      meta: [
+        { name: "description", content: this.$page.post.tagsMeta },
 
-				{
-					key: "og:image",
-					property: "og:image",
-					content: "https://alojanaklik.rs" + this.$page.post.imageTwo.path
-				},
-				{
-					key: "og:title",
-					name: "og:title",
-					content: this.$page.post.title + " - Aloja Vera"
-				},
+        {
+          key: "og:image",
+          property: "og:image",
+          content: "https://alojanaklik.rs" + this.$page.post.imageTwo.path
+        },
+        {
+          key: "og:title",
+          name: "og:title",
+          content: this.$page.post.title + " - Aloja Vera"
+        },
 
-				{
-					key: "og:type",
-					name: "og:type",
-					content: "article"
-				},
+        {
+          key: "og:type",
+          name: "og:type",
+          content: "article"
+        },
 
-				{
-					key: "og:description",
-					name: "og:description",
-					content: this.$page.post.tagsMeta
-				},
-				{
-					key: "twitter:title",
-					name: "twitter:title",
-					content: this.$page.post.title + " - Aloja Vera"
-				},
-				{
-					key: "twitter:description",
-					name: "twitter:description",
-					content: this.$page.post.tagsMeta
-				},
-				{
-					key: "twitter:image",
-					property: "twitter:image",
-					content: "https://alojanaklik.rs" + this.$page.post.imageTwo.path
-				}
-			]
+        {
+          key: "og:description",
+          name: "og:description",
+          content: this.$page.post.tagsMeta
+        },
+        {
+          key: "twitter:title",
+          name: "twitter:title",
+          content: this.$page.post.title + " - Aloja Vera"
+        },
+        {
+          key: "twitter:description",
+          name: "twitter:description",
+          content: this.$page.post.tagsMeta
+        },
+        {
+          key: "twitter:image",
+          property: "twitter:image",
+          content: "https://alojanaklik.rs" + this.$page.post.imageTwo.path
+        }
+      ]
 
-			// etc...
-		};
-	},
-	data: function() {
-		return {
-			api_url: "https://alojanaklik.rs/",
-			facebook: "http://www.facebook.rs/sharer/sharer.php?u="
-		};
-	},
+      // etc...
+    };
+  },
+  data: function() {
+    return {
+      api_url: "https://alojanaklik.rs/",
+      facebook: "http://www.facebook.rs/sharer/sharer.php?u="
+    };
+  },
 
-	//CodyHouse javascript UI
-	mounted() {
-		let frontEnd = document.createElement("script");
-		frontEnd.setAttribute("src", "../../main-header.js");
-		frontEnd.setAttribute("id", "main-header-js");
+  //CodyHouse javascript UI
+  mounted() {
+    let frontEnd = document.createElement("script");
+    frontEnd.setAttribute("src", "../../main-header.js");
+    frontEnd.setAttribute("id", "main-header-js");
 
-		document.body.appendChild(frontEnd);
-	},
-	destroyed() {
-		document.getElementById("main-header-js").remove();
-	}
+    document.body.appendChild(frontEnd);
+  },
+  destroyed() {
+    document.getElementById("main-header-js").remove();
+  }
 };
 </script>
+
+
